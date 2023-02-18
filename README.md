49edc0e5de4dab9ae3d63521960d883973e06e1b
Simon Paul <samonpaul06@gmail.com>
2023-02-18 13:09:33 +0530

    vite upgrade

--- README.md
+++
@@ -1,6 +0,0 @@
-# Hablar - Group Chat App
-
--- React and Firebase 
--- Google Authentication
-
-LIVE DEMO -- https://hablar-chat-app.netlify.app/
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      Vite app
+    </div>
+  )
+}
+
+export default App
