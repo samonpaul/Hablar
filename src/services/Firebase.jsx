ade76d344e89861c0f71feaab83ebf5cadd007fe
Simon Paul <samonpaul06@gmail.com>
2023-02-21 23:46:54 +0530

    firebase issue

--- src/services/Firebase.jsx
+++ src/services/Firebase.jsx
@@ -13,4 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db = getFirestore(app, {
+  experimentalForceLongPolling: true
+  })
